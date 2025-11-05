b14c133178e66bf80bc217da573bd517795532b6
MOHANPATELBG <mohanpatel.1991@gmail.com>
2025-11-05 22:33:05 +0530

    Practiced few basic programs

---
+++ loopingStatments.js
@@ -0,0 +1,12 @@
+//looping statmemnts 
+//for loop
+for (let i = 1; i <= 5; i++) {
+    console.log('Iteration number: ' + i);
+}
+
+//while loop
+let j = 1;
+while (j <= 20) {
+    console.log('While loop iteration: ' + j);
+    j++;
+}
